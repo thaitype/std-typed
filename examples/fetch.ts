6e4ad84693341e95a12930af886e5b1e2cd98af0
Thada Wangthammang <thada.wth@gmail.com>
2024-06-07 19:30:39 +0700

    feat(example): error handling class

--- examples/fetch.ts
+++ examples/fetch.ts
@@ -1,31 +1,70 @@
+import type { Err } from "src/Result.js";
 import { Result, Std } from "std-typed";
+import { match } from "ts-pattern";
 
 /**
  * https://www.npmjs.com/package/p-retry
  * https://2024-effect-days-keynote.vercel.app/8
  * https://youtu.be/PxIBWjiv3og
+ *
+ * TODO: Unhandled throw Error, is missing
  */
-type GetTodoError = "Failed to fetch todo";
+
+// export class FetchError {
+//   constructor(
+//     public readonly kind: "FetchError" | "InvalidJsonError" | "RequestFailError"
+//   ) {}
+// }
+class FetchError extends Error {
+  constructor(
+    public readonly kind:
+      | "FetchError"
+      | "InvalidJsonError"
+      | "RequestFailError",
+    error?: unknown
+  ) {
+    const message =
+      error instanceof Error ? error.message : String(error ?? "");
+    super(message);
+    if (error instanceof Error) {
+      this.stack = error.stack;
+      this.cause = error.cause;
+      this.name = error.name;
+    }
+  }
+}
 
 const getTodo = (id: number) =>
-  Result.funcAsync<number, GetTodoError>(async c => {
-    const result = await fetch(
-      `https://jsonplaceholder.typicode.com/todos/${id}`
-    );
-    if (!result.ok) return c.err("Failed to fetch todo");
-    const json = await result.json();
-    return c.ok(json);
+  Result.funcAsync<unknown, FetchError>(async c => {
+    try {
+      const result = await fetch(
+        `https://jsonplaceholder.typicode.com/todos/${id}`
+      );
+      if (!result.ok) return c.err(new FetchError("FetchError"));
+
+      try {
+        const json = await result.json();
+        return c.ok(json);
+      } catch (e) {
+        return c.err(new FetchError("InvalidJsonError", e));
+      }
+    } catch (e) {
+      return c.err(new FetchError("RequestFailError", e));
+    }
   });
-/**
- * Delay for a specified amount of time
- * @param ms value in milliseconds
- * @returns Promise of void
- */
-export const delay = (ms: number) => new Promise(res => setTimeout(res, ms));
 
 Std.runExit(async () => {
   for (const id of [-1, 1]) {
     const result = await getTodo(id);
-    console.log(`Fetch Result (id="${id}"): => ${result}`);
+    match(result.into())
+      .with({ _tag: "success" }, value =>
+        console.log(
+          `Fetch Result (id="${id}"): => ${JSON.stringify(value.value)}`
+        )
+      )
+      .with({ _tag: "failure"}, error =>
+        console.error(`Fetch Error (id="${id}"): => ${error.error} ${error.error.kind}`)
+      )
+      .exhaustive();
   }
 });
